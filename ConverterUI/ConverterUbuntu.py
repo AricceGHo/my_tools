a5b2cc6f742830795bb12fe540b3325a7a6f565c
AricceGHo <ariccer@yandex.ru>
2019-08-13 09:30:11 +0300

    Доработка интерфейса

--- ConverterUI/ConverterUbuntu.py
+++ ConverterUI/ConverterUbuntu.py
@@ -1,43 +1,100 @@
+# -*- coding:utf-8 -*-
 import os
 import sys
 from PyQt5 import QtWidgets
 import ConverterUbuntuUI
-
+import json
+from PyQt5.QtGui import QIcon
 
 class ExampleApp(QtWidgets.QMainWindow, ConverterUbuntuUI.Ui_Form):
 
     def button_click_open(self):
         filters = "User Interface (*.ui)"
         f_dir = os.path.dirname(__file__)
-        self.file_name = QtWidgets.QFileDialog.getOpenFileName(
+        self.file_name_ui = QtWidgets.QFileDialog.getOpenFileName(
             None, "Выберете UI файл", f_dir, filters)
         
 
     def button_click_convert(self):
         filters = "Py файлы (*.py)"
         f_dir = os.path.dirname(__file__)
-        file_name = QtWidgets.QFileDialog.getSaveFileName(
+        self.file_name_py = QtWidgets.QFileDialog.getSaveFileName(
             None, "Назовите файл", f_dir, filters)
-        print(file_name[0])
-        file_name_f, file_extention= os.path.splitext(file_name[0])
-        print(file_extention)
+        
+        file_name_f, file_extention= os.path.splitext(self.file_name_py[0])
+       
         if file_extention =='.py':
-            file_name_f=file_name[0]
+            self.file_name_f=self.file_name_py[0]
         
         else :
-            file_name_f = file_name[0]+".py"
+            self.file_name_f = self.file_name_py[0]+".py"
        
 
-        #print(file_name_f)
-        bash_command="pyuic5 %s -o %s" %(self.file_name[0],file_name_f)
+        
+        bash_command="pyuic5 %s -o %s" %(self.file_name_ui[0],self.file_name_f)
         os.system(bash_command)
+    def button_click_save(self):
+        try:
+            json_string = {"%s"%(self.name_lineEdit.text()):{"ui_path": "%s" % (self.file_name_ui[0]), "py_path": "%s" % (self.file_name_py[0])}}
+            filters = "JSON файлы (*.json)"
+            f_dir = os.path.dirname(__file__)
+            file_name = QtWidgets.QFileDialog.getSaveFileName(
+                None, "Назовите файл настроек", f_dir, filters)
+            file_name_f, file_extention = os.path.splitext(file_name[0])
+            json_string_n=[[]]
+            if file_extention == '.json':
+                file_name_f = file_name[0]
+
+            else:
+                file_name_f = file_name[0]+".json"
+            json_string_n.append(json_string)
+            print(json_string_n)
+            with open(file_name_f, "w") as write_file:
+                
+                json.dump(json_string_n, write_file)
+
+        except Exception as e:
+            print(e)
+            print('tuta')
+
+    def button_click_load(self):
+        
+        try:
+            filters = "JSON файлы (*.json)"
+            f_dir = os.path.dirname(__file__)
+            file_name = QtWidgets.QFileDialog.getOpenFileName(
+                None, "Выберете файл настроек", f_dir, filters)
+            with open(file_name[0], 'r') as read_file:
+                self.settings = json.load(read_file)
+            self.file_name_ui=self.settings["ui_path"]
+            self.file_name_py=self.settings["py_path"]
+            print('ok')
+            
+        except(TypeError, FileNotFoundError):
+            self.comboBox.addItem('Отмена загрузки файла настроек')
+        except Exception as e:
+            print(e)
+
+    def same_folder_checkbox(self):
+        print()
+    
 
     def __init__(self):
 
         super().__init__()
         self.setupUi(self)
+        self.initUI()
         self.openButton.clicked.connect(self.button_click_open)
         self.converButton.clicked.connect(self.button_click_convert)
+        self.saveButton.clicked.connect(self.button_click_save)
+        self.loadButton.clicked.connect(self.button_click_load)
+        self.sameBox.stateChanged.connect(self.same_folder_checkbox)
+
+    def initUI(self):
+        f_dir = os.path.dirname(__file__)
+        self.setWindowIcon(QIcon(f_dir+'/reload.svg'))
+        self.saveButton.setIcon(QIcon(f_dir+'/save.png'))
+        
 
 
 def main():
--- ConverterUI/ConverterUbuntuUI.py
+++ ConverterUI/ConverterUbuntuUI.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/ariccegho/git/PythonProjects/ConverterUI/ConverterUbuntu.ui'
+# Form implementation generated from reading ui file '/home/ariccegho/mytools/ConverterUI/ConverterUbuntu.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
@@ -13,15 +13,40 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(183, 91)
+        Form.resize(378, 119)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("reload.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Form.setWindowIcon(icon)
         self.openButton = QtWidgets.QPushButton(Form)
         self.openButton.setGeometry(QtCore.QRect(10, 10, 161, 25))
         self.openButton.setObjectName("openButton")
         self.converButton = QtWidgets.QPushButton(Form)
         self.converButton.setGeometry(QtCore.QRect(10, 50, 161, 25))
         self.converButton.setObjectName("converButton")
+        self.sameBox = QtWidgets.QCheckBox(Form)
+        self.sameBox.setGeometry(QtCore.QRect(190, 50, 111, 23))
+        self.sameBox.setObjectName("sameBox")
+        self.loadButton = QtWidgets.QPushButton(Form)
+        self.loadButton.setGeometry(QtCore.QRect(180, 10, 51, 25))
+        self.loadButton.setObjectName("loadButton")
+        self.saveButton = QtWidgets.QPushButton(Form)
+        self.saveButton.setGeometry(QtCore.QRect(350, 10, 21, 25))
+        self.saveButton.setAcceptDrops(False)
+        self.saveButton.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("save.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.saveButton.setIcon(icon1)
+        self.saveButton.setObjectName("saveButton")
+        self.name_lineEdit = QtWidgets.QLineEdit(Form)
+        self.name_lineEdit.setGeometry(QtCore.QRect(240, 10, 113, 25))
+        self.name_lineEdit.setObjectName("name_lineEdit")
+        self.comboBox = QtWidgets.QComboBox(Form)
+        self.comboBox.setGeometry(QtCore.QRect(10, 80, 161, 25))
+        self.comboBox.setCurrentText("")
+        self.comboBox.setObjectName("comboBox")
 
         self.retranslateUi(Form)
+        self.comboBox.setCurrentIndex(-1)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
@@ -29,3 +54,6 @@ class Ui_Form(object):
         Form.setWindowTitle(_translate("Form", "ConverterPYQT"))
         self.openButton.setText(_translate("Form", "Открыть файл .ui"))
         self.converButton.setText(_translate("Form", "Конвертировать в .py"))
+        self.sameBox.setText(_translate("Form", "Та же папка"))
+        self.loadButton.setText(_translate("Form", " Load"))
+        self.name_lineEdit.setText(_translate("Form", "Название"))
